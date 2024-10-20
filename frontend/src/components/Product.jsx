efed4534a32f50fd64ad4f1e857204bc86fd4939
pasadem <pasadem@ukr.net>
2024-10-20 23:04:53 +0300

    imageUploadFix

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
   return (
-    <Card className=" my-2 p-3 rounded hight-200px">
+    <Card className=" my-2 p-3 rounded " style={{ height: 350}}>
       <Link to={`/product/${product._id}`}>
         <Card.Img  src={product.image} variant="top" />
       </Link>
       <Card.Body as="div">
         <Link to={`/product/${product._id}`}>
-          <Card.Title >
+          <div>
             <h6><strong>{product.name}</strong></h6>
-          </Card.Title>
+          </div>
         </Link>
         <Card.Text as="div">Виробник: {product.producer}</Card.Text>
         <Card.Text as="h5">${product.price}</Card.Text>
