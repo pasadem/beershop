6e73d4e2e2f28ecd1e942eacb5ee3e1f8fac5911
pasadem <pasadem@ukr.net>
2024-10-27 23:01:38 +0200

    image-upload

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
   return (
-    <Card className=" my-2 rounded " style={{ height: 250 }}>
+    <Card className=" my-2 rounded " style={{ height: 280 }}>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" style={{ height: 150 }}/>
+        <Card.Img src={product.image} variant="top" style={{ height: 180 }}/>
       </Link>
       <Card.Body>
       <Card.Title as="strong">{product.name}</Card.Title>
 
         <Card.Text as="div">Виробник: {product.origin}</Card.Text>
-        <Card.Text as="h5">{product.price} грн</Card.Text>
+        <Card.Text as="h6">{product.price} грн</Card.Text>
       </Card.Body>
     </Card>
   );
