b28f15f58bf6200e3e46ee483d410d7a5395105f
pasadem <pasadem@ukr.net>
2024-10-29 17:28:38 +0200

    image upload fixing

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.get('/api/config/paypal', (req, res) =>
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use('/images', express.static('/var/data/uploads'));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
