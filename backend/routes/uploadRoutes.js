0d2e58f626eb48ee4b6958b7ddc8a614838369f7
pasadem <pasadem@ukr.net>
2024-10-29 16:29:43 +0200

    image upload fixing

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'frontend/public/images/');
+    cb(null, 'uploads/');
   },
   filename(req, file, cb) {
     cb(
